eb5bb330e2b4ad6c1dfb955b763fc9058eaa4668
KruglovV <vlad_kruglov@list.ru>
2021-05-17 15:02:51 +0400

    Added a api routes

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('books', 'App\Http\Controllers\Api\BookController@getAllBooks');
+Route::get('books/{id}', 'App\Http\Controllers\Api\BookController@getBook');
+Route::post('books', 'App\Http\Controllers\Api\BookController@createBook');
+Route::put('books/{id}', 'App\Http\Controllers\Api\BookController@updateBook');
+Route::delete('books/{id}', 'App\Http\Controllers\Api\BookController@deleteBook');
