b4d3add8340dad30d9799d55088db4674b8367fd
KruglovV <vlad_kruglov@list.ru>
2021-05-18 15:20:48 +0400

    Ended of main part app

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\BookController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,8 +19,8 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('books', 'App\Http\Controllers\Api\BookController@getAllBooks');
-Route::get('books/{id}', 'App\Http\Controllers\Api\BookController@getBook');
-Route::post('books', 'App\Http\Controllers\Api\BookController@createBook');
-Route::put('books/{id}', 'App\Http\Controllers\Api\BookController@updateBook');
-Route::delete('books/{id}', 'App\Http\Controllers\Api\BookController@deleteBook');
+Route::get('books', [BookController::class, 'getAllBooks']);
+Route::get('books/{id}', [BookController::class, 'getBook']);
+Route::post('books', [BookController::class, 'createBook']);
+Route::put('books/{id}', [BookController::class, 'updateBook']);
+Route::delete('books/{id}', [BookController::class, 'deleteBook']);
