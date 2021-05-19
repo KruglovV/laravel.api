a9df7757ebe6405825493500d0d128543d54aa24
KruglovV <vlad_kruglov@list.ru>
2021-05-19 23:10:45 +0400

    Added AuthConroller to api folder. Edit route, added route to register method from AuthController

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\BookController;
+use App\Http\Controllers\Api\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,6 +19,8 @@ use App\Http\Controllers\Api\BookController;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/register', [AuthController::class, 'register']);
 
 Route::get('books', [BookController::class, 'getAllBooks']);
 Route::get('books/{id}', [BookController::class, 'getBook']);
